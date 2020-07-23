2342c8571ecff13c3c719f62338954760b6f74d8
Diego Mazega <diego.mazega@live.com>
2020-07-23 11:27:08 -0300

    Primeira Classe de Dominio

--- src/main/java/com/diegomazega/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/diegomazega/cursomc/resources/CategoriaResources.java
@@ -1,16 +1,26 @@
 package com.diegomazega.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.diegomazega.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias") //nome do endpoint
 public class CategoriaResources {
 	
 	@GetMapping
-	public String listar() {
-		return "Rest ok";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		return list;
 	}
 
 }
