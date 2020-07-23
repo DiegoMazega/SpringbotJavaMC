aa6cca29ba7ef3186f19ffc8d707df53fed301e6
Diego Mazega <diego.mazega@live.com>
2020-07-23 13:46:43 -0300

    Criando operação de instanciação

--- src/main/java/com/diegomazega/cursomc/CursoMcApplication.java
+++ src/main/java/com/diegomazega/cursomc/CursoMcApplication.java
@@ -1,13 +1,31 @@
 package com.diegomazega.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoMcApplication {
+import com.diegomazega.cursomc.domain.Categoria;
+import com.diegomazega.cursomc.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoMcApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		service.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
