27e50ccbfe1d96f351e9212f02a7776cb653ad02
Diego Mazega <diego.mazega@live.com>
2020-07-29 14:14:36 -0300

    Estado e Cidade

--- src/main/java/com/diegomazega/cursomc/CursoMcApplication.java
+++ src/main/java/com/diegomazega/cursomc/CursoMcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.diegomazega.cursomc.domain.Categoria;
+import com.diegomazega.cursomc.domain.Cidade;
+import com.diegomazega.cursomc.domain.Estado;
 import com.diegomazega.cursomc.domain.Produto;
 import com.diegomazega.cursomc.repositories.CategoriaRepository;
+import com.diegomazega.cursomc.repositories.CidadeRepository;
+import com.diegomazega.cursomc.repositories.EstadoRepository;
 import com.diegomazega.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -20,6 +24,12 @@ public class CursoMcApplication implements CommandLineRunner {
 	@Autowired
 	private ProdutoRepository produtoRepository;
 	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+		
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
 	}
@@ -40,9 +50,22 @@ public class CursoMcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().add(cat1);
 		
-		
 		service.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().add(c1);
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
 	}
 
 }
--- src/main/java/com/diegomazega/cursomc/domain/Estado.java
+++ src/main/java/com/diegomazega/cursomc/domain/Estado.java
@@ -8,62 +8,49 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import javax.persistence.OneToMany;
 
 @Entity
-public class Produto implements Serializable {
+public class Estado implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY )
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
 	private String nome;
-	private Double preco;
 	
-	@JsonBackReference
-	@ManyToMany
-	@JoinTable(name = "PRODUTO_CATEGORIA", 
-	joinColumns = @JoinColumn(name = "produto_id"),
-	inverseJoinColumns = @JoinColumn(name = "categoria_id"))
-	private List<Categoria> categorias = new ArrayList<>();
+	@OneToMany(mappedBy = "estado")
+	private List<Cidade> cidades = new ArrayList<>();
 	
-	public Produto(){}
-	public Produto(Integer id, String nome, Double preco) {
+	public Estado() {}
+	
+	public Estado(Integer id, String nome) {
 		this.id = id;
 		this.nome = nome;
-		this.preco = preco;
 	}
+	
 	public Integer getId() {
-		return id;
+		return this.id;
 	}
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
 	public String getNome() {
-		return nome;
+		return this.nome;
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public Double getPreco() {
-		return preco;
+
+	public List<Cidade> getCidades() {
+		return cidades;
 	}
-	public void setPreco(Double preco) {
-		this.preco = preco;
+
+	public void setCidades(List<Cidade> cidades) {
+		this.cidades = cidades;
 	}
-	
-	public List<Categoria> getCategorias() {
-		return categorias;
-	}
-	public void setCategorias(List<Categoria> categorias) {
-		this.categorias = categorias;
-	}
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -71,6 +58,7 @@ public class Produto implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -79,7 +67,7 @@ public class Produto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Produto other = (Produto) obj;
+		Estado other = (Estado) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -87,5 +75,4 @@ public class Produto implements Serializable {
 			return false;
 		return true;
 	}
-	
 }
